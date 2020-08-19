e30cdcf07744b0707e7b66067ec022b68d813ce0
Harsha4517 <67313604+Harsha4517@users.noreply.github.com>
2020-08-19 21:10:35 +0530

    Day_06_Problem_Solution
    commiting solution

---
+++ Day_06_Problem_Find_All_Duplicates_In_An_Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        d={}
+        l=[]
+        for i in nums:
+            if i in d:
+                l.append(i)
+            else:
+                d[i]=1
+        return l
